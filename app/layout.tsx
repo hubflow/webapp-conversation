4855cde853f0ff097098db924944e1013ca9f542
Kunjesh Kumar <kunjeshkumar@Kunjeshs-MacBook-Air.local>
2024-05-07 21:13:42 +0530

    fix: responsive issue fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import { getLocaleOnServer } from '@/i18n/server'
-import { Montserrat, Poppins } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
 import './styles/globals.css'
 import './styles/markdown.scss'
-import { Viewport } from 'next/dist/lib/metadata/types/extra-types'
-import { Metadata } from 'next'
-
+import type { Viewport } from 'next/dist/lib/metadata/types/extra-types'
+import type { Metadata } from 'next'
 
 const montserrat = Montserrat({
   weight: ['400', '500', '700'],
@@ -19,12 +18,12 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   viewportFit: 'cover',
-  userScalable: false
+  userScalable: false,
 }
 
 export const metadata: Metadata = {
   title: 'Chatflo AI',
-  viewport: viewport
+  viewport,
 }
 const LocaleLayout = ({
   children,
@@ -34,9 +33,9 @@ const LocaleLayout = ({
   const locale = getLocaleOnServer()
   return (
     <html lang={locale ?? 'en'} className={`h-full ${montserrat.className}`}>
-      <body className="h-full overflow-y-auto">
+      <body className="h-full overflow-y-hidden">
         <div className="overflow-x-auto">
-          <div className="w-screen h-screen min-w-[300px]">
+          <div className="w-screen h-[90vh] sm:h-[90vh] md:h-screen lg:h-screen min-w-[300px]">
             {children}
           </div>
         </div>
